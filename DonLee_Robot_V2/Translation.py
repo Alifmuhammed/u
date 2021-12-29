9d13d14ba30d5fea75e73947a84c3b97bcd4ff2b
Alifmuhammed <95561254+Alifmuhammed@users.noreply.github.com>
2021-12-29 13:19:41 +0530

    Update Translation.py

--- DonLee_Robot_V2/Translation.py
+++ DonLee_Robot_V2/Translation.py
@@ -6,33 +6,18 @@
 
 class Text(object):
     START_TEXT = """
-👋𝖧𝖾𝗒  {}, Iam <a href="t.me/Donlee_Robot">𝖣𝗈𝗇𝖫𝖾𝖾 𝖱𝗈𝖻𝗈𝗍 𝖵2</a>
-𝖬𝖺𝗄𝖾 𝗆𝖾 𝖺𝗇 𝖺𝖽𝗆𝗂𝗇 𝖿𝗈𝗋 𝗒𝗈𝗎𝗋 𝗀𝗋𝗈𝗎𝗉 𝖺𝗇𝖽 𝖼𝗁𝖺𝗇𝗇𝖾𝗅 𝗍𝗁𝖾𝗇 𝖼𝗈𝗇𝗇𝖾𝖼𝗍 𝗆𝖾....🎉
-🙂𝖳𝗈 𝖻𝗎𝗂𝗅𝖽 𝖺 𝖣𝗈𝗇𝖫𝖾𝖾 𝗋𝗈𝖻𝗈𝗍 𝖵2 𝗅𝗂𝗄𝖾 𝗆𝖾, 𝖼𝗅𝗂𝖼𝗄 𝗈𝗇 𝗍𝗁𝖾 /sub 𝖼𝗈𝗆𝗆𝖺𝗇𝖽
-𝖲𝗎𝖻𝗌𝖼𝗋𝗂𝖻𝖾 𝗍𝗈 𝗍𝗁𝖾 𝗎𝗉𝖽𝖺𝗍𝖾 𝖼𝗁𝖺𝗇𝗇𝖾𝗅 𝗍𝗈 𝗅𝖾𝖺𝗋𝗇 𝖺𝖻𝗈𝗎𝗍 𝗆𝗒 𝗎𝗉𝖽𝖺𝗍𝖾𝗌 𝖺𝗇𝖽 𝖺𝖼𝗍𝗂𝗏𝗂𝗍𝗒...
-𝖢𝗅𝗂𝖼𝗄 𝗈𝗇 𝗍𝗁𝖾 𝖧𝖾𝗅𝗉 /help 𝖻𝗎𝗍𝗍𝗈𝗇 𝖿𝗈𝗋 𝗆𝗈𝗋𝖾 𝗂𝗇𝖿𝗈𝗋𝗆𝖺𝗍𝗂𝗈𝗇
-🧒 𝖬𝖺𝗂𝗇𝗍𝖺𝗂𝗇𝖾𝖽 𝖡𝗒 <a href="https://t.me/Mrk_yt">𝖳𝗁𝗂𝗌 𝖯𝖾𝗋𝗌𝗈𝗇</a> & <a href="tg://user?id={}">𝖳𝗁𝗂𝗌 𝖯𝖾𝗋𝗌𝗈𝗇</a>       
+👋𝖧𝖾𝗒  {}, Iam <a href="http://t.me/Legend_boy_robot">Boy</a>       
 """
     HELP_TEXT = """
-<b><u>𝖡𝖠𝖲𝖨𝖢 𝖢𝖮𝖬𝖬𝖠𝖭𝖣𝖲</u></b>
-
-• /start, alive :- 𝖢𝗁𝖾𝖼𝗄 𝖨𝖺𝗆 𝖠𝗅𝗂𝗏𝖾
-• /help :- 𝖬𝗈𝗋𝖾 𝖣𝖾𝗍𝖺𝗂𝗅𝗌
-• /about :- 𝖠𝖻𝗈𝗎𝗍 𝖬𝖾
-• /sub :- 𝖲𝗎𝗉𝗉𝗈𝗋𝗍 𝖺𝗇𝖽 𝖽𝖾𝗉𝗅𝗈𝗒
-• /send, /broadcast :- 𝖡𝗈𝗍 𝖣𝖾𝗏𝖾𝗅𝗈𝗉𝖾𝗋 𝖮𝗇𝗅𝗒
-
-- 𝖠𝗅𝗅 𝖼𝗈𝗆𝗆𝖺𝗇𝖽𝗌 𝖼𝖺𝗇 𝖾𝗂𝗍𝗁𝖾𝗋 𝖻𝖾 𝗎𝗌𝖾𝖽 𝗐𝗂𝗍𝗁 /
-- 𝖠𝗇𝖽 𝗍𝗁𝖾 𝖿𝗈𝗅𝗅𝗈𝗐𝗂𝗇𝗀
+<b> Here my commands</b>
 """
     ABOUT_TEXT = """
-📃 𝖡𝗈𝗍 : <a href="t.me/{}">𝖣𝗈𝗇𝖫𝖾𝖾 𝖱𝗈𝖻𝗈𝗍 V2</a>
-🧒 𝖣𝖾𝗏 : <a href="t.me/Mrk_yt">𝖬𝗎𝗁𝖺𝗆𝗆𝖾𝖽</a>
-🧒 𝖢𝗋𝖾𝖺𝗍𝗈𝗋 : <a href="tg://user?id={}">{}</a>
+📃 𝖡𝗈𝗍 : <a href="http://t.me/Legend_boy_robot">Boy</a>
+🧒 𝖣𝖾𝗏 : <a href="t.me/Alifmuhammad_tg">Alif Muhammed</a>
 🗣️ 𝖫𝖺𝗇𝗀𝗎𝖺𝗀𝖾 : <a href="https://docs.pyrogram.org">𝖯𝗒𝗍𝗁𝗈𝗇3</a>
 📚 𝖫𝗂𝖻𝗋𝖺𝗋𝗒 : <a href="https://docs.pyrogram.org">𝖯𝗒𝗋𝗈𝗀𝗋𝖺𝗆 𝖠𝗌𝗒𝗇𝖼𝗂𝗈 1.13.0</a>
 🗒️ 𝖵𝖾𝗋𝗌𝗂𝗈𝗇 : <a href="t.me/{}">5.0.0 [𝖡𝖾𝗍𝖺]</a>
-📖 𝖲𝗈𝗎𝗋𝖼𝖾 𝖢𝗈𝖽𝖾 : <a href="https://youtu.be/NrbMc93aCzA">𝖢𝗅𝗂𝖼𝗄 𝖧𝖾𝗋𝖾</a>
+📖 𝖲𝗈𝗎𝗋𝖼𝖾 𝖢𝗈𝖽𝖾 : <a href="http://t.me/Legend_boy_robot">𝖢𝗅𝗂𝖼𝗄 𝖧𝖾𝗋𝖾</a>
 """
     SUB_TEXT = """
 𝖧𝖾𝗅𝗅𝗈 𝖿𝗋𝗂𝖾𝗇𝖽𝗌,
@@ -57,10 +42,8 @@ class Text(object):
 𝖦𝗈 𝗊𝗎𝗂𝖼𝗄𝗅𝗒 𝖺𝗇𝖽 𝗉𝗂𝖼𝗄 𝗂𝗍 𝗎𝗉
 """
     SPELLING_TEXT = """
-𝖧𝖾𝗅𝗅𝗈 {},
-𝖢𝗈𝗎𝗅𝖽𝗇'𝗍 𝖥𝗂𝗇𝖽 {} ? 𝖯𝗅𝖾𝖺𝗌𝖾 𝖢𝗅𝗂𝖼𝗄 𝖸𝗈𝗎𝗋 𝖱𝖾𝗊𝗎𝖾𝗌𝗍 𝖬𝗈𝗏𝗂𝖾 𝖭𝖺𝗆𝖾
-
-{} മൂവി കിട്ടിയില്ല അല്ലെ ? നിങ്ങൾ ടൈപ്പ് ചെയ്ത സ്പ്ലിങ് നോക്കു..!!
+<b>𝖧𝖾𝗅𝗅𝗈 {},
+𝖢𝗈𝗎𝗅𝖽𝗇'𝗍 𝖥𝗂𝗇𝖽 {} ? 𝖯𝗅𝖾𝖺𝗌𝖾 𝖢𝗅𝗂𝖼𝗄 𝖸𝗈𝗎𝗋 𝖱𝖾𝗊𝗎𝖾𝗌𝗍 𝖬𝗈𝗏𝗂𝖾 𝖭𝖺𝗆𝖾 correct spelling </b>
 """
     AUTO_FILTER_1_TEXH = """
 𝖧𝖾𝗅𝗉: <b><u>𝖠𝗎𝗍𝗈 𝖥𝗂𝗅𝗍𝖾𝗋
